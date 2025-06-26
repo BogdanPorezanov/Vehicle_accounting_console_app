0f657c6d3872f70f47e4199bc55f76b2e3c03ef0
ПК_User <_User@DESKTOP-IPPEFIJ>
2025-06-26 21:41:51 +0300

    Добавьте файлы проекта.

---
+++ include/Vehicle_accounting_console_app.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+//#include 
+
+inline void Parse_console_input();
+
+class Vehicle
+{
+public:
+	Vehicle(std::string, std::string, int, int); // марка, модель, год выпуска, вес
+	~Vehicle();
+	int Get_id() { return id; }
+	int Get_id_counter() { return id_counter; }
+
+protected:
+	std::string Type_of_vehicle;
+private:
+	static int id_counter;
+
+	int id;
+	std::string Brand;
+	std::string Model;
+	int Year_of_release;
+	int Weight;
+
+};
+
+class Car : public Vehicle
+{
+public:
+	Car(std::string, std::string, int, int, int, int); // + число владельцев, пробег
+	~Car();
+private:
+	int Number_of_owners;
+	int Mileage;
+};
---
+++ src/Vehicle_accounting_console_app.cpp
@@ -0,0 +1,58 @@
+﻿#include "Vehicle_accounting_console_app.h"
+
+
+int Vehicle::id_counter = 0;
+
+
+int main()
+{
+	/*Vehicle v("suz", "sem", 1, 2);
+	Vehicle vz("suz", "sem", 1, 2);
+	Car c("suz", "sem", 1, 2, 3, 4);
+	std::cout << vz.Get_id() << std::endl;
+	std::cout << c.Get_id() << std::endl;
+
+	std::cout << vz.Get_id_counter() << std::endl;
+	std::cout << c.Get_id_counter() << std::endl;
+
+	Car cz("suz", "sem", 1, 2, 3, 4);
+	Vehicle vx("suz", "sem", 1, 2);
+	std::cout << vx.Get_id_counter() << std::endl;
+	std::cout << cz.Get_id_counter() << std::endl;*/
+
+	while (1)
+	{
+		Parse_console_input();
+	}
+	
+	//return 0;
+}
+
+inline void Parse_console_input()
+{
+
+}
+
+Vehicle::Vehicle(std::string Brand, std::string Model, int Year_of_release, int Weight)
+{
+	this->id = ++(this->id_counter);
+	this->Brand = Brand;
+	this->Model = Model;
+	this->Year_of_release = Year_of_release;
+	this->Weight = Weight;
+}
+
+Vehicle::~Vehicle()
+{
+}
+
+Car::Car(std::string Brand, std::string Model, int Year_of_release, int Weight, int Number_of_owners, int Mileage) :Vehicle(Brand, Model, Year_of_release, Weight)
+{
+	this->Type_of_vehicle = "car";
+	this->Number_of_owners = Number_of_owners;
+	this->Mileage = Mileage;
+}
+
+Car::~Car()
+{
+}
