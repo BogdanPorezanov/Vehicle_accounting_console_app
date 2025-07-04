ed12534df398feef8b1fa2fe7b6b4644a7d138a7
ПК_User <bogdan.porezanov@yandex.ru>
2025-07-04 09:56:09 +0300

    добавил геттеры

--- include/Vehicle.h
+++ include/Vehicle.h
@@ -9,17 +9,24 @@ public:
 	~Vehicle();
 	int Get_id() { return id; }
 	int Get_id_counter() { return id_counter; }
+	virtual std::string Get_Type() = 0;
+	std::string Get_Brand() { return Brand; }
+	std::string Get_Model() { return Model; }
+	int Get_Year_of_release() { return Year_of_release; }
+	int Get_Weight() { return Weight; }
 
 protected:
-	std::string Type;
-private:
-	static int id_counter;
+	//std::string Type;
 
 	int id;
 	std::string Brand;
 	std::string Model;
 	int Year_of_release;
 	int Weight;
+private:
+	static int id_counter;
+
+	
 
 };
 
@@ -28,6 +35,10 @@ class Car : public Vehicle
 public:
 	Car(std::string, std::string, int, int, int, int); // + число владельцев, пробег
 	~Car();
+	std::string Get_Type() override { return "Car"; }
+	int Get_Number_of_owners() { return Number_of_owners; }
+	int Get_Mileage() { return Mileage; }
+
 private:
 	int Number_of_owners;
 	int Mileage;
@@ -38,6 +49,9 @@ class Boat : public Vehicle
 public:
 	Boat(std::string, std::string, int, int, int, int); // + водоизмещение, глубина погружения винта
 	~Boat();
+	std::string Get_Type() override { return "Boat"; }
+	int Get_Displacement() { return Displacement; }
+	int Get_Propeller_immersion_depth() { return Propeller_immersion_depth; }
 
 private:
 	int Displacement;
@@ -49,6 +63,9 @@ class Airplane : public Vehicle
 public:
 	Airplane(std::string, std::string, int, int, int, int); // + грузоподъёмность, размах крыла
 	~Airplane();
+	std::string Get_Type() override { return "Airplane"; }
+	int Get_Cargo_capacity() { return Cargo_capacity; }
+	int Get_Wingspan() { return Wingspan; }
 
 private:
 	int Cargo_capacity;
@@ -60,6 +77,9 @@ class Spaceship : public Vehicle
 public:
 	Spaceship(std::string, std::string, int, int, std::string, int); // + грузоподъёмность, размах крыла
 	~Spaceship();
+	std::string Get_Type() override { return "Spaceship"; }
+	std::string Get_Fuel_type() { return Fuel_type; }
+	int Get_Hyperjump_range() { return Hyperjump_range; }
 
 private:
 	std::string Fuel_type;
